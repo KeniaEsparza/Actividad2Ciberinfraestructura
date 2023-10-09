83077bfe407d4bf42accb4e50e0fe9b847330b87
Keniss <71326502+KeniaEsparza@users.noreply.github.com>
2023-10-09 11:22:24 -0600

    Add files via upload

---
+++ MySQLConexion/MySQLConexion/Form1.Designer.cs
@@ -0,0 +1,184 @@
+﻿namespace MySQLConexion
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            btnConectar = new Button();
+            label1 = new Label();
+            label2 = new Label();
+            txtUsuario = new TextBox();
+            txtContrasena = new TextBox();
+            txtHost = new TextBox();
+            txtBasededatos = new TextBox();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            txtPuerto = new TextBox();
+            dgvDatos = new DataGridView();
+            ((System.ComponentModel.ISupportInitialize)dgvDatos).BeginInit();
+            SuspendLayout();
+            // 
+            // btnConectar
+            // 
+            btnConectar.Location = new Point(134, 232);
+            btnConectar.Name = "btnConectar";
+            btnConectar.Size = new Size(75, 23);
+            btnConectar.TabIndex = 0;
+            btnConectar.Text = "Conectar";
+            btnConectar.UseVisualStyleBackColor = true;
+            btnConectar.Click += btnConectar_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(12, 38);
+            label1.Name = "label1";
+            label1.Size = new Size(50, 15);
+            label1.TabIndex = 1;
+            label1.Text = "Usuario:";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(12, 78);
+            label2.Name = "label2";
+            label2.Size = new Size(70, 15);
+            label2.TabIndex = 2;
+            label2.Text = "Contraseña:";
+            // 
+            // txtUsuario
+            // 
+            txtUsuario.Location = new Point(87, 38);
+            txtUsuario.Name = "txtUsuario";
+            txtUsuario.Size = new Size(179, 23);
+            txtUsuario.TabIndex = 3;
+            // 
+            // txtContrasena
+            // 
+            txtContrasena.Location = new Point(87, 75);
+            txtContrasena.Name = "txtContrasena";
+            txtContrasena.Size = new Size(179, 23);
+            txtContrasena.TabIndex = 4;
+            // 
+            // txtHost
+            // 
+            txtHost.Location = new Point(87, 117);
+            txtHost.Name = "txtHost";
+            txtHost.Size = new Size(179, 23);
+            txtHost.TabIndex = 5;
+            // 
+            // txtBasededatos
+            // 
+            txtBasededatos.Location = new Point(87, 158);
+            txtBasededatos.Name = "txtBasededatos";
+            txtBasededatos.Size = new Size(179, 23);
+            txtBasededatos.TabIndex = 6;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(37, 125);
+            label3.Name = "label3";
+            label3.Size = new Size(35, 15);
+            label3.TabIndex = 7;
+            label3.Text = "Host:";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(-1, 161);
+            label4.Name = "label4";
+            label4.Size = new Size(83, 15);
+            label4.TabIndex = 8;
+            label4.Text = "Base de Datos:";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(37, 201);
+            label5.Name = "label5";
+            label5.Size = new Size(45, 15);
+            label5.TabIndex = 9;
+            label5.Text = "Puerto:";
+            // 
+            // txtPuerto
+            // 
+            txtPuerto.Location = new Point(87, 193);
+            txtPuerto.Name = "txtPuerto";
+            txtPuerto.Size = new Size(179, 23);
+            txtPuerto.TabIndex = 10;
+            // 
+            // dgvDatos
+            // 
+            dgvDatos.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dgvDatos.Location = new Point(272, 38);
+            dgvDatos.Name = "dgvDatos";
+            dgvDatos.RowTemplate.Height = 25;
+            dgvDatos.Size = new Size(415, 217);
+            dgvDatos.TabIndex = 11;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(dgvDatos);
+            Controls.Add(txtPuerto);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(txtBasededatos);
+            Controls.Add(txtHost);
+            Controls.Add(txtContrasena);
+            Controls.Add(txtUsuario);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(btnConectar);
+            Name = "Form1";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)dgvDatos).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button btnConectar;
+        private Label label1;
+        private Label label2;
+        private TextBox txtUsuario;
+        private TextBox txtContrasena;
+        private TextBox txtHost;
+        private TextBox txtBasededatos;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private TextBox txtPuerto;
+        private DataGridView dgvDatos;
+    }
+}
